7be57806e77bf05dbc36a460c2c99c222868a096
Richard Willis <willis.rh@gmail.com>
2021-12-04 10:56:18 +0000

    Create AWS stack

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,142 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import fs from 'node:fs';
+import path from 'node:path';
+import { setFailed, debug, notice, getInput } from '@actions/core';
+import {
+  CloudFormationClient,
+  ListStacksCommand,
+  StackSummary,
+} from '@aws-sdk/client-cloudformation';
+import * as ansi from './ansi';
+
+const cfTemplateBody = fs.readFileSync(
+  path.resolve(
+    __dirname,
+    '..',
+    'cloudformation',
+    's3bucket_with_cloudfront.yml'
+  ),
+  'utf8'
+);
+
+async function getAllStacks(
+  client: CloudFormationClient,
+  nextToken?: string,
+  allStacks: StackSummary[] = []
+): Promise<StackSummary[]> {
+  const command = new ListStacksCommand({
+    NextToken: nextToken,
+  });
+  const response = await client.send(command);
+  const stacks = allStacks.concat(response.StackSummaries || []);
+  if (response.NextToken) {
+    return getAllStacks(client, response.NextToken, stacks);
+  }
+  debug(`Found ${stacks.length} stacks`);
+  return stacks;
+}
+
+async function getExistingStack(
+  client: CloudFormationClient,
+  cfStackName: string
+): Promise<StackSummary | void> {
+  debug(`Searching for existing stack with name: ${cfStackName}`);
+  const allStacks = await getAllStacks(client);
+  return allStacks.find((stack) => stack.StackName === cfStackName);
+}
+
+async function hasCreatedStack(
+  client: CloudFormationClient,
+  cfStackName: string
+): Promise<boolean> {
+  const stack = await getExistingStack(client, cfStackName);
+  return stack !== undefined;
+}
+
+async function updateExistingStack(
+  client: CloudFormationClient,
+  cfStackName: string
+): Promise<void> {
+  notice(`${ansi.green}Updating existing stack, this can take a while...`);
+  // const command = new UpdateStackCommand({
+  //   StackName: cfStackName,
+  //   TemplateBody: cfTemplateBody,
+  // });
+  // return client.send(command);
+}
+
+async function createNewStack(
+  client: CloudFormationClient,
+  cfStackName: string
+): Promise<void> {
+  notice('\u001b[32mCreating new stack, this can take a while...');
+  // const command = new CreateStackCommand({
+  //   StackName: cfStackName,
+  //   TemplateBody: cfTemplateBody,
+  // });
+  // return client.send(command);
+}
+
+function getInputs() {
+  const cfStackName = getInput('cfStackName', {
+    required: true,
+    trimWhitespace: true,
+  });
+  const s3BucketName = getInput('s3BucketName', {
+    required: true,
+    trimWhitespace: true,
+  });
+  const s3AllowedOrigins = getInput('s3AllowedOrigins', {
+    required: true,
+    trimWhitespace: true,
+  });
+  const rootCloudFrontHosts = getInput('rootCloudFrontHosts', {
+    required: true,
+    trimWhitespace: true,
+  });
+  const previewCloudFrontHosts = getInput('previewCloudFrontHosts', {
+    required: true,
+    trimWhitespace: true,
+  });
+  const cacheCorsPathPattern = getInput('cacheCorsPathPattern', {
+    required: true,
+    trimWhitespace: true,
+  });
+  const certificateARN = getInput('certificateARN', {
+    required: true,
+    trimWhitespace: true,
+  });
+
+  return {
+    cfStackName,
+    s3BucketName,
+    s3AllowedOrigins,
+    rootCloudFrontHosts,
+    previewCloudFrontHosts,
+    cacheCorsPathPattern,
+    certificateARN,
+  };
+}
 
 async function run(): Promise<void> {
+  const client = new CloudFormationClient({ region: 'us-east-1' });
+
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const inputs = getInputs();
+    const hasExistingStack = await hasCreatedStack(client, inputs.cfStackName);
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    debug(`Found existing stack: ${String(hasExistingStack)}`);
 
-    core.setOutput('time', new Date().toTimeString())
+    if (hasExistingStack) {
+      await updateExistingStack(client, inputs.cfStackName);
+    } else {
+      await createNewStack(client, inputs.cfStackName);
+    }
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      setFailed(error.message);
+    }
+  } finally {
   }
 }
 
-run()
+void run();
